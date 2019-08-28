9056929ae9a6b6bdd89b5e87a97cd94ed110c26a
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-08-28 01:51:57 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,11 @@
-  class Song
+  class Song  
+      attr_accessor :artist :title
+      
+      def initialize 
+        @artist = artist
+        @title = title
+      end
+      
+      class author
+            @post = post
+      
